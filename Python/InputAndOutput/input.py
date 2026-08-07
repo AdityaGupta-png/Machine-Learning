efc05568e2b1e812dc53e25374a873711704c9cc
Aditya <adityagupta64837@gmail.com>
2026-08-07 15:03:39 +0530

    Take input from user

---
+++ Python/InputAndOutput/input.py
@@ -0,0 +1,11 @@
+# Taking the input 
+
+# Example on the formating string 
+name = "Aditya"
+age = 21
+
+print(f"My name is {name} and age is {age}")
+
+# Taking the input from the user
+name = str(input("What is your name : "))
+print(f"Hello, I'm {name}! Welcome to python programming")
