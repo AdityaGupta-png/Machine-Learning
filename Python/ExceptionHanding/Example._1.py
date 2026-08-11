768078a1d8f743e973bd25f01072d261a0e134e1
Aditya <adityagupta64837@gmail.com>
2026-08-11 13:28:14 +0530

    Exceptional handling completed

---
+++ Python/ExceptionHanding/Example._1.py
@@ -0,0 +1,45 @@
+
+# Example number 1 
+
+# a = int(input("Enter the number : "))
+
+# try :
+#     print(10/a)
+    
+# except Exception as err:
+#     print(f"sorry there is an exception of {err} ")
+    
+
+# print("Successfully done ")
+
+
+# # Example number 2
+
+# #  divide the number by string
+
+# n = str(input("Enter your name  : "))
+
+# try :
+#     print(10/n)
+# except Exception as err:
+#     print(f"sorry the error is {err}")
+    
+# print("Successfully done")
+
+
+# Exaamples numbers 3 
+
+n1 = str(input("Enter the number : "))
+
+try :
+    print(10/n1)
+    
+except Exception as err :
+    print(f"the error is {err}")
+    
+else :
+    print('Done operation')
+    
+finally :
+   print(44 + 33)
+
